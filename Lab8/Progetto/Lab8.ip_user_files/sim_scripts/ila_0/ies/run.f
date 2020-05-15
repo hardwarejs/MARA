e088b15a291d7b6f8a2d806370a4cad087788d3d
​Alice Pagano <alice.pagano@studenti.unipd.it>
2020-05-15 21:09:51 +0200

    update

--- Lab8/Progetto/Lab8.ip_user_files/sim_scripts/ila_0/ies/run.f
+++
@@ -1,4 +0,0 @@
--makelib ies_lib/xil_defaultlib \
-  "../../../../Lab8.srcs/sources_1/ip/ila_0/sim/ila_0.vhd" \
--endlib
-
--- Lab8/Progetto/Lab8.ip_user_files/sim_scripts/ila_0/xcelium/run.f
+++
@@ -1,4 +0,0 @@
--makelib xcelium_lib/xil_defaultlib \
-  "../../../../Lab8.srcs/sources_1/ip/ila_0/sim/ila_0.vhd" \
--endlib
-
